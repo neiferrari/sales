68b619980f675e5f887a4a86018e9be7eed56f5c
NeiFerrari <nei.ferrari@gmail.com>
2023-05-02 20:12:35 +0200

    3rd main.py: Step 2

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     return """<h1>Api Prediction:</h1>
-              <h2>Ad Investment to Sales Prediction.</h2>
-              <hr>Add TV, Radio & Newspaper Investments on /api/v1/predict. <b>INT VALUES</b></hr>"""
+              <h2>1. Ad Investment to Sales Prediction.</h2>
+              <hr>Add TV, Radio & Newspaper Investments on /api/v1/predict. <b>INT VALUES</b></hr>
+              <hr></hr>
+              <h2>2. New Data</h2>
+              <hr>Add New Values of TV, Radio & Newspaper Investments AND Sales Results on /api/v1/retrain.</hr>       
+              """
 
 @app.route('/api/v1/predict', methods=['GET'])
 def predict():
@@ -22,15 +26,16 @@ def predict():
                 newspaper]
 
     prediction = loaded_model.predict([new_data])
-    return jsonify({'Prediction': prediction[0]})
+    return jsonify({'Prediction': round(prediction[0],2)})
 
-# @app.route('/api/v1/retrain', methods=['POST'])
-# def retrain():
-#     sale = request.get_json()
-#     tv = sale['tv']
-#     radio = sale['radio']
-#     newspaper = sale['newspaper']
-#     return jsonify({'message': 'OK, Added Book'})
+app.route('/api/v1/retrain', methods=['POST'])
+def retrain():
+    sale = request.get_json()
+    tv = sale['tv']
+    radio = sale['radio']
+    newspaper = sale['newspaper']
+    sales = sale['sales']
+    return jsonify({'message': 'OK, Added New Data'})
 
 if __name__ == '__main__':
     app.run(debug=True) 
